31a6f76ee709cbabe31d9e0d63a247c40dfc3079
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-09-12 16:20:23 -0500

    Use generics for the solution

--- linked_lists/delete_middle_node/solution.go
+++ linked_lists/delete_middle_node/solution.go
@@ -2,7 +2,7 @@ package deletemiddlenode
 
 import linkedlists "github.com/sarrietav-dev/interview-practice/linked_lists"
 
-func Delete(node *linkedlists.Node[string]) string {
+func Delete[T comparable](node *linkedlists.Node[T]) T {
 	if node.Next.Next == nil {
 		nextValue := node.Next.Value
 		node.Next = nil
