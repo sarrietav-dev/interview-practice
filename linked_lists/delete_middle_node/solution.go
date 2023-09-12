124b1b5c2f0e936dc7ef55b582032456383f70c5
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-09-12 16:36:16 -0500

    Add documentation

--- linked_lists/delete_middle_node/solution.go
+++ linked_lists/delete_middle_node/solution.go
@@ -2,6 +2,7 @@ package deletemiddlenode
 
 import linkedlists "github.com/sarrietav-dev/interview-practice/linked_lists"
 
+// Delete deletes a node in the middle of a linked list, given only access to that node.
 func Delete[T comparable](node *linkedlists.Node[T]) T {
 	if node.Next.Next == nil {
 		nextValue := node.Next.Value
