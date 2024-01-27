2709b7f92face163a79cd01cb3e23f2e3f8d7a90
Sebastian <25210925+sarrietav-dev@users.noreply.github.com>
2024-01-27 17:06:30 -0500

    Format Two Sum

--- hash_tables/two_sum/two_sum.go
+++ hash_tables/two_sum/two_sum.go
@@ -2,19 +2,19 @@ package twosum
 
 // https://leetcode.com/problems/two-sum
 func TwoSum(nums []int, target int) []int {
-  numMap := make(map[int]int)
+	numMap := make(map[int]int)
 
-  for i, v := range nums {
-    needed := target - v
+	for i, v := range nums {
+		needed := target - v
 
-    index, exists := numMap[needed]
+		index, exists := numMap[needed]
 
-    if exists {
-      return []int {i, index}
-    }
+		if exists {
+			return []int{i, index}
+		}
 
-    numMap[v] = i
-  }
+		numMap[v] = i
+	}
 
-  return nil
+	return nil
 }
--- hash_tables/two_sum/two_sum_test.go
+++ hash_tables/two_sum/two_sum_test.go
@@ -13,41 +13,40 @@ func TestTwoSumOne(t *testing.T) {
 		2, 7, 11, 15,
 	}, 9
 
-  result := twosum.TwoSum(nums, target)
-  
-  if !compare(result, []int{0,1}) {
-    t.Errorf("Expected {0, 1} but got %v", result)
-  }
+	result := twosum.TwoSum(nums, target)
+
+	if !compare(result, []int{0, 1}) {
+		t.Errorf("Expected {0, 1} but got %v", result)
+	}
 }
 
 func TestTwoSumTwo(t *testing.T) {
-  nums, target := []int{
-    3, 2, 4,
-  }, 6
+	nums, target := []int{
+		3, 2, 4,
+	}, 6
 
-  result := twosum.TwoSum(nums, target)
-  
-  if !compare(result, []int{1,2}) {
-    t.Errorf("Expected {1, 2} but got %v", result)
-  }
+	result := twosum.TwoSum(nums, target)
+
+	if !compare(result, []int{1, 2}) {
+		t.Errorf("Expected {1, 2} but got %v", result)
+	}
 }
 
 func TestTwoSumThree(t *testing.T) {
-  nums, target := []int{
-    3, 3,
-  }, 6
+	nums, target := []int{
+		3, 3,
+	}, 6
 
-  result := twosum.TwoSum(nums, target)
-  
-  if !compare(result, []int{0,1}) {
-    t.Errorf("Expected {0, 1} but got %v", result)
-  }
+	result := twosum.TwoSum(nums, target)
+
+	if !compare(result, []int{0, 1}) {
+		t.Errorf("Expected {0, 1} but got %v", result)
+	}
 }
-
 
 // Compares the result slice with the expected slice and its reverse
 func compare[T cmp.Ordered](result []T, expected []T) bool {
-  reverced := slices.Clone(expected)
-  slices.Reverse(reverced)
-  return slices.Compare(result, expected) == 0 || slices.Compare(result, reverced) == 0;
+	reverced := slices.Clone(expected)
+	slices.Reverse(reverced)
+	return slices.Compare(result, expected) == 0 || slices.Compare(result, reverced) == 0
 }
